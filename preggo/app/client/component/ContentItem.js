4c49bcf1971f7e5e43a7c75ac1c6a551d4c9ab0a
Halie <halie.creollo@gmail.com>
2019-08-04 01:36:48 -0500

    edit

--- preggo/app/client/component/ContentItem.js
+++ preggo/app/client/component/ContentItem.js
@@ -1,16 +1,22 @@
-import React, {Component} from 'react';
-import {Card} from 'reactstrap';
+import React, { Component } from 'react';
+import { Card } from 'reactstrap';
 
+class ContentItem extends Component {
+    render() {
+        return (
 
-<Card className="bg-dark text-white">
-  <Card.Img src="holder.js/100px270" alt="Card image" />
-  <Card.ImgOverlay>
-    <Card.Title>Preggo</Card.Title>
-    <Card.Text>
-    A nutritional app for pregnant women.
-    </Card.Text>
-    <Card.Text> To get started, enter the profile information to calculate your nutrient goals per the USDA's recommendation.</Card.Text>
-  </Card.ImgOverlay>
-</Card>
+            <Card className="bg-dark text-white">
+                <Card.Img src="holder.js/100px270" alt="Card image" />
+                <Card.ImgOverlay>
+                    <Card.Title>Preggo</Card.Title>
+                    <Card.Text>
+                        A nutritional app for pregnant women.
+                     </Card.Text>
+                    <Card.Text> To get started, enter the profile information to calculate your nutrient goals per the USDA's recommendation.</Card.Text>
+                </Card.ImgOverlay>
+            </Card>
+        );
+    }
+}
 
-  export default ContentItem
+export default ContentItem
